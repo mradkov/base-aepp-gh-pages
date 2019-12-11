efa4fa72791141f83c3d50527b9434e10155b65f
Milen Radkov <milenradkov@me.com>
2019-12-11 21:50:41 +0100

    Add files via upload

---
+++ precache-manifest.ab648c2a43acb37d98b933b17b7211c7.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0fdd338519b83759668",
+    "url": "/js/ui-desktop~ui-mobile.aa14c66f.js"
+  },
+  {
+    "revision": "4e0bf62fff3d290dc19c",
+    "url": "/js/analytics.eec3f1fd.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "bcc8cf907c13fe251968",
+    "url": "/js/chunk-01bf26c6.e97220ae.js"
+  },
+  {
+    "revision": "0a090d837d187717311e",
+    "url": "/js/ui-mobile.cb46f01e.js"
+  },
+  {
+    "revision": "3d4b72d650d93064d531",
+    "url": "/js/chunk-49c39293.75306119.js"
+  },
+  {
+    "revision": "5193a48d824bced057d7",
+    "url": "/js/app.c5a386fb.js"
+  },
+  {
+    "revision": "5154d6a183ee029eb7da",
+    "url": "/js/chunk-cdf88a9e.d7f28992.js"
+  },
+  {
+    "revision": "5e44eef1919ef8a3d71d",
+    "url": "/js/chunk-vendors.db15af65.js"
+  },
+  {
+    "revision": "3a2728e4b89f66cf44f8",
+    "url": "/js/locale-cn.3043376e.js"
+  },
+  {
+    "revision": "4649408f082692ca6640",
+    "url": "/js/locale-es.28bb8890.js"
+  },
+  {
+    "revision": "5f2c022e49e44b9f45d9",
+    "url": "/js/locale-ru.4966f202.js"
+  },
+  {
+    "revision": "77519f6d272bfda1cc04",
+    "url": "/js/ui-desktop.e0692f73.js"
+  },
+  {
+    "revision": "694e729598866d1efa6a",
+    "url": "/js/page-apps.93e85ef8.js"
+  },
+  {
+    "revision": "946356ae72b5b7b9a301",
+    "url": "/js/ui.744f1a40.js"
+  },
+  {
+    "revision": "3882e760f3916b79c8f3",
+    "url": "/js/page-redeem.95da7334.js"
+  },
+  {
+    "revision": "d7af20e0c398ca2af870",
+    "url": "/js/page-redeem~sdk.8b21f53a.js"
+  },
+  {
+    "revision": "76a2eaf3388379715619",
+    "url": "/js/socket-io.45db1081.js"
+  },
+  {
+    "revision": "eb8aea1560e2d7f30635",
+    "url": "/js/popup.b7a7dae7.js"
+  },
+  {
+    "revision": "5187349f611caf858150",
+    "url": "/js/sdk.b7555404.js"
+  },
+  {
+    "revision": "0f2ddd082f4123980724",
+    "url": "/js/popup~ui.afd587db.js"
+  },
+  {
+    "revision": "591a09b04f748cd8135a",
+    "url": "/js/qr-codes.5722ab40.js"
+  },
+  {
+    "revision": "df4725986dbd8b0753994a50089090c2",
+    "url": "/img/1f4b8.df472598.png"
+  },
+  {
+    "revision": "71720cdec2387e5154fc795dc6e75d33",
+    "url": "/favicons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "c97f08aaa6825c7a003b107c4a40b2b1",
+    "url": "/favicons/apple-touch-icon.png"
+  },
+  {
+    "revision": "616f2b6bdebd12def4b4e47849be59ad",
+    "url": "/ios.webmanifest"
+  },
+  {
+    "revision": "1a292ec0ea571ef3ede2fd2d2d4aab3c",
+    "url": "/favicons/mstile-150x150.png"
+  },
+  {
+    "revision": "704ed8ed1bce8bc62a32786e9bd8d0b9",
+    "url": "/favicons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "341fbd7df81086774c441021b455824d",
+    "url": "/favicons/favicon-32x32.png"
+  },
+  {
+    "revision": "1554e330cbea80ffcefa62d17580e800",
+    "url": "/favicons/favicon-16x16.png"
+  },
+  {
+    "revision": "0257b8e99414cd5037fee7728aca0ff8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9641ea92ea6366591118d800696eb5bf",
+    "url": "/img/subaccounts.9641ea92.svg"
+  },
+  {
+    "revision": "3c46f67bc714c42408a5b33f869ad582",
+    "url": "/img/1f570-fe0f.3c46f67b.png"
+  },
+  {
+    "revision": "eff43f357aa920e984cbd8d1dc3a04b2",
+    "url": "/img/1f513.eff43f35.png"
+  },
+  {
+    "revision": "596601c39600749d0749400e49398e7f",
+    "url": "/img/phones.596601c3.svg"
+  },
+  {
+    "revision": "91c0efe6083681b964896f952e7ac1fa",
+    "url": "/img/aepps.91c0efe6.svg"
+  },
+  {
+    "revision": "0f259ddae95110bb9dcc12d2f5e68535",
+    "url": "/img/account-card.0f259dda.svg"
+  },
+  {
+    "revision": "5e984ef9f9447c54689e7de0664025ad",
+    "url": "/img/1f91e.5e984ef9.png"
+  },
+  {
+    "revision": "1bc3720145ff0a20b6274a53969ce626",
+    "url": "/img/1f31f.1bc37201.png"
+  },
+  {
+    "revision": "64be39b0b6c586fab38b5c8a5ced4798",
+    "url": "/img/1f44b.64be39b0.png"
+  },
+  {
+    "revision": "89398478a4c6c59ec6baf72433d9a474",
+    "url": "/img/1f4ab.89398478.png"
+  },
+  {
+    "revision": "ac29b61fb221df85b0923347326ef0ed",
+    "url": "/img/1f481-200d-2642-fe0f.ac29b61f.png"
+  },
+  {
+    "revision": "32e51fdc70c744ae9325b7671258105e",
+    "url": "/img/send.32e51fdc.svg"
+  },
+  {
+    "revision": "01057309838a32bed2e7d9ea9ec80848",
+    "url": "/img/1f511.01057309.png"
+  },
+  {
+    "revision": "9c019846d037e02bdfd2b012d602be46",
+    "url": "/img/270d-fe0f.9c019846.png"
+  },
+  {
+    "revision": "2455e0a04b5f528d63747f50cd8d9e5a",
+    "url": "/img/1f4f2.2455e0a0.png"
+  },
+  {
+    "revision": "77519f6d272bfda1cc04",
+    "url": "/css/ui-desktop.c73c05b1.css"
+  },
+  {
+    "revision": "591a09b04f748cd8135a",
+    "url": "/css/qr-codes.3eaa61e0.css"
+  },
+  {
+    "revision": "0a090d837d187717311e",
+    "url": "/css/ui-mobile.11dc5757.css"
+  },
+  {
+    "revision": "cdc77607c9d2e9abcb0a7689f1fc5c79",
+    "url": "/favicons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "d0fdd338519b83759668",
+    "url": "/css/ui-desktop~ui-mobile.554bd70e.css"
+  },
+  {
+    "revision": "e9464aea3ffea1b783c6f4cffd61eff7",
+    "url": "/default.webmanifest"
+  },
+  {
+    "revision": "946356ae72b5b7b9a301",
+    "url": "/css/ui.cc12f751.css"
+  },
+  {
+    "revision": "0f2ddd082f4123980724",
+    "url": "/css/popup~ui.6b09bc08.css"
+  },
+  {
+    "revision": "eb8aea1560e2d7f30635",
+    "url": "/css/popup.0eefedaf.css"
+  },
+  {
+    "revision": "3882e760f3916b79c8f3",
+    "url": "/css/page-redeem.685fb9fc.css"
+  },
+  {
+    "revision": "694e729598866d1efa6a",
+    "url": "/css/page-apps.c04c67ce.css"
+  },
+  {
+    "revision": "5154d6a183ee029eb7da",
+    "url": "/css/chunk-cdf88a9e.69649ec1.css"
+  },
+  {
+    "revision": "3d4b72d650d93064d531",
+    "url": "/css/chunk-49c39293.e685ef96.css"
+  },
+  {
+    "revision": "bcc8cf907c13fe251968",
+    "url": "/css/chunk-01bf26c6.2bc1a1d9.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,42 @@
+importScripts("/precache-manifest.ab648c2a43acb37d98b933b17b7211c7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/* global workbox clients */
+/* eslint no-restricted-globals: 0 */
+// eslint-disable-next-line no-underscore-dangle
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+self.addEventListener('push', (event) => {
+  const title = 'Remote device requests to sign a transaction';
+  const options = {
+    body: 'Confirm transaction signing in Base æpp',
+    icon: 'favicons/android-chrome-192x192.png',
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+
+  event.waitUntil((async () => {
+    const clientList = await clients.matchAll({
+      includeUncontrolled: true,
+      type: 'window',
+    });
+    if (clientList.length) {
+      await clientList[0].focus();
+      return;
+    }
+
+    await clients.openWindow('/');
+  })());
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data.action === 'skipWaiting') {
+    event.waitUntil(self.skipWaiting());
+  }
+});
+
+self.addEventListener('activate', event => event
+  .waitUntil(self.clients.claim()));
+
